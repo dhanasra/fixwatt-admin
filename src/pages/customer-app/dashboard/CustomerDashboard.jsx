fb806338d56c4b69524cd9e5909d7af9aad6e557
dhanasra <dhana.spiderlingz@gmail.com>
2024-08-23 22:52:11 +0530

    update: youtube videos added

--- src/pages/customer-app/dashboard/CustomerDashboard.jsx
+++ src/pages/customer-app/dashboard/CustomerDashboard.jsx
@@ -185,6 +185,69 @@ const CustomerDashboard = ()=>{
       <div ref={sectionRefs.testimonials}>
         <Testimonials/>
       </div>
+
+      <Stack sx={{alignItems: "center"}}>
+        <Grid container spacing={6} sx={{maxWidth: "1000px"}}>
+          <Grid item xs={6}>
+            <Box
+              sx={{
+                position: 'relative',
+                paddingBottom: '56.25%', /* 16:9 aspect ratio */
+                height: 0,
+                overflow: 'hidden',
+                maxWidth: '100%',
+                backgroundColor: '#000',
+                borderRadius: 2,
+                boxShadow: 1,
+              }}
+            >
+              <iframe
+                src={`https://www.youtube.com/embed/0IBKSUjRz4Q`}
+                frameBorder="0"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                allowFullScreen
+                title="Embedded YouTube"
+                style={{
+                  position: 'absolute',
+                  top: 0,
+                  left: 0,
+                  width: '100%',
+                  height: '100%',
+                }}
+              />
+            </Box>
+          </Grid>
+          <Grid item xs={6}>
+            <Box
+                sx={{
+                  position: 'relative',
+                  paddingBottom: '56.25%', /* 16:9 aspect ratio */
+                  height: 0,
+                  overflow: 'hidden',
+                  maxWidth: '100%',
+                  backgroundColor: '#000',
+                  borderRadius: 2,
+                  boxShadow: 1,
+                }}
+              >
+                <iframe
+                  src={`https://www.youtube.com/embed/b53-xbzO8tU`}
+                  frameBorder="0"
+                  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                  allowFullScreen
+                  title="Embedded YouTube"
+                  style={{
+                    position: 'absolute',
+                    top: 0,
+                    left: 0,
+                    width: '100%',
+                    height: '100%',
+                  }}
+                />
+              </Box>
+          </Grid>
+        </Grid>
+      </Stack>
       
       <div ref={sectionRefs.contact}>
         <Contact/>
